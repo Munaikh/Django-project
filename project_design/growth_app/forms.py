7bb0a9c25f3b4cfe43b48e446e73bcd785fd8cba
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-03-05 12:26:04 +0000

    sign in page

--- project_design/growth_app/forms.py
+++ project_design/growth_app/forms.py
@@ -1,14 +1,43 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm as DjangoPasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm as DjangoPasswordChangeForm, AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Business, SalesData, UserProfile
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add Bootstrap classes to form fields
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs.update({'class': 'form-control'})
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        
+        if commit:
+            user.save()
+            # Create a UserProfile for the new user
+            UserProfile.objects.create(user=user)
+            
+        return user
+
+class SignInForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    
+    class Meta:
+        model = User
+        fields = ('username', 'password')
 
 class BusinessForm(forms.ModelForm):
     class Meta:
